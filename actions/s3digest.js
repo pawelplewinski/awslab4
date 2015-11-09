0164ba58957b486d968580a12f9a8199d1e627c4
Your Name <you@example.com>
2015-11-09 11:27:35 +0100

    checksum

--- actions/s3digest.js
+++ actions/s3digest.js
@@ -1,9 +1,29 @@
 var AWS = require("aws-sdk");
+//var url = require('url');
 
 AWS.config.loadFromPath('./config.json');
 
 var task =  function(request, callback){
-
+	params = request.query;
+	console.log(request.query);
+	AWS.config.loadFromPath('./config.json');
+	var s3 = new AWS.S3();
+	s3.getObject(
+	{bucket: params.bucket, key: params.key},
+	function(error, data) {
+		if (error)
+		{
+			callback(error);
+			return;
+		}
+		var digest = require('helpers');
+		var checksum = digest.calculateDigest('sha-1', data);
+		callback(null, checksum);
+	});
+		
+	callback("error");
+	//var url_parts = url.parse(request.url, true);
+	
 	
 	
 }

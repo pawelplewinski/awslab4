2aa2fb636f228fcd6885ab1b95935c4363f37304
Your Name <you@example.com>
2015-11-09 10:42:40 +0100

    gsdgasdgasdgasdg

--- actions/createform.js
+++ actions/createform.js
@@ -18,12 +18,13 @@ var task = function(request, callback){
 	//3. generate form fields for S3 POST
 	var s3Form = new S3Form(policy);
 	//4. get bucket name
+	bucketName = policyData.conditions["bucket"]
 	policy.generateSignature(awsConfig.secretAccessKey);
 	fields = s3Form.generateS3FormFields();
 	s3Form.addS3CredientalsFields(fields, awsConfig);
 
 	console.log(s3Form.fields)
-
+	console.log(bucketName)
 	callback(null, {template: INDEX_TEMPLATE, params:{fields:fields, bucket:""}});
 }
 

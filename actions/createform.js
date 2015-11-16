5bd1a91b6aa6655e25d2432a6650e24e939d5029
adrian.andrzejewski <prami@o2.pl>
2015-11-16 11:42:42 +0100

    lab5

--- actions/createform.js
+++ actions/createform.js
@@ -6,6 +6,8 @@ var AWS_CONFIG_FILE = "config.json";
 var POLICY_FILE = "policy.json";
 var INDEX_TEMPLATE = "index.ejs";
 
+var AWS = require("aws-sdk");
+AWS.config.loadFromPath('./config.json');
 
 var task = function(request, callback){
 	//1. load configuration
@@ -18,9 +20,60 @@ var task = function(request, callback){
 	//3. generate form fields for S3 POST
 	var s3Form = new S3Form(policy);
 	//4. get bucket name
+	var bucket = policy.getConditionValueByKey("bucket")
 	
+	var form = s3Form.generateS3FormFields();
+	
+	var credentials = s3Form.addS3CredientalsFields(form, awsConfig)
 
-	callback(null, {template: INDEX_TEMPLATE, params:{fields:[], bucket:""}});
+	var simpledb = new AWS.SimpleDB();
+	
+	var date = new Date().toString();
+	
+	var domain = 'aandrzejewski'
+	
+	
+	var dbParams = {
+		Attributes: [
+		{
+			Name: "Type",
+			Value: "generateForm"
+		},
+		{
+			Name: "Date",
+			Value: date
+		},
+		{
+			Name: "Browser",
+			Value: request.headers.host
+		},
+		{
+			Name: "User-Agent",
+			Value: request.headers['user-agent']
+		}
+		],
+		DomainName: domain,
+		ItemName: "logItem"
+	};
+	
+	var createDomain = function(){
+		simpledb.createDomain({DomainName:domain}, function(err, data) {
+		  if (err) console.log(err, err.stack); // an error occurred
+		  else     console.log("Domain created!");           // successful response
+		});
+	}; 
+	
+	simpledb.domainMetadata({DomainName:domain}, function(err, data) {
+	  if (err)  createDomain();// an error occurred
+	  else     console.log("Domain exists");           // successful response
+	});
+	
+	simpledb.putAttributes(dbParams, function(err, data) {
+	  if (err) console.log(err, err.stack); // an error occurred
+	  else     console.log("Data logged");           // successful response
+	});
+	
+	callback(null, {template: INDEX_TEMPLATE, params:{fields:form, bucket:bucket}});
 }
 
 exports.action = task;
--- actions/s3digest.js
+++ actions/s3digest.js
@@ -3,8 +3,18 @@ var AWS = require("aws-sdk");
 AWS.config.loadFromPath('./config.json');
 
 var task =  function(request, callback){
-
+	callback(null, JSON.stringify(request.query));
 	
+	var s3= new AWS.S3();
+	var params={
+		Bucket: request.query.bucket,
+		Key: request.query.key
+	};
+	
+	s3.getObject(params, function(err,data){
+		if (err) console.log(err);
+		else console.log(data);
+	});
 	
 }
 
